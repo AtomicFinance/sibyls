5cc8f43261436d0fe49ccb6f5676ea43f580fc19
Matthew Black <mblackmblackmblack@gmail.com>
2023-08-31 23:30:13 -0400

    --wip-- [skip ci]

--- src/oracle/pricefeeds/mod.rs
+++ src/oracle/pricefeeds/mod.rs
@@ -6,6 +6,7 @@ pub use error::PriceFeedError;
 pub use error::Result;
 pub use gateio::GateIo;
 pub use kraken::Kraken;
+pub use deribit::Deribit;
 
 use crate::oracle::pricefeeds::PriceFeedError::InternalError;
 use crate::AssetPair;
@@ -15,17 +16,18 @@ mod error;
 #[async_trait]
 pub trait PriceFeed {
     fn id(&self) -> &'static str;
-    fn translate_asset_pair(&self, asset_pair: AssetPair) -> &'static str;
+    fn translate_asset_pair(&self, asset_pair: AssetPair) -> Result<&'static str>;
     async fn retrieve_price(&self, asset_pair: AssetPair, datetime: OffsetDateTime) -> Result<f64>;
 }
 
-pub static ALL_PRICE_FEEDS: &'static [&str] = &["bitstamp", "gateio", "kraken"];
+pub static ALL_PRICE_FEEDS: &'static [&str] = &["bitstamp", "gateio", "kraken", "deribit"];
 
 pub fn create_price_feed(feed_id: &str) -> Result<Box<dyn PriceFeed + Send + Sync>> {
     match feed_id {
         "bitstamp" => Ok(Box::new(Bitstamp {})),
         "gateio" => Ok(Box::new(GateIo {})),
         "kraken" => Ok(Box::new(Kraken {})),
+        "deribit" => Ok(Box::new(Deribit {})),
         _ => Err(InternalError(format!("unknown price feed {}", feed_id))),
     }
 }
@@ -33,3 +35,4 @@ pub fn create_price_feed(feed_id: &str) -> Result<Box<dyn PriceFeed + Send + Syn
 mod bitstamp;
 mod gateio;
 mod kraken;
+mod deribit;
